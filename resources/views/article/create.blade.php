2f58e72894e4b7a9c0c1c2de863861490423a80d
nhan <huynhnhan.dev@gmail.com>
2020-03-25 21:17:16 +0700

    extends form layout master

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -1,16 +1,18 @@
+@extends('article.layouts.master')
+@section('title','Tao bai viet')
+@section('page_content')
 <h1>Tao bai viet</h1>
 <form action="{{ route('createArticle') }}" method="post">
     <!-- Neu khong co token cho route nhan thi no se bi loi 419 -->
     {{ csrf_field() }}
-    <div>
-        <label>Tieu de</label>
-        <input type="text" name="title">
+    <div class="form-group">
+        <label for="title">Tieu de</label>
+        <input type="text" class="form-control" placeholder="Enter title" name="title">
     </div>
-    <div>
-        <label>Noi dung</label>
-        <textarea name="content"></textarea>
+    <div class="form-group">
+        <label for="content">Noi dung</label>
+        <textarea class="form-control" rows="5" name="content"></textarea>
     </div>
-    <div>
-        <button type="submit">Dang bai</button>
-    </div>
+    <button type="submit" class="btn btn-primary">Dang Bai</button>
 </form>
+@endsection
