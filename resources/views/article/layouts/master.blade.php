aeaba606b003346de4ddb2a993fcb206b1af76c0
nhan <huynhnhan.dev@gmail.com>
2020-03-25 21:19:43 +0700

    add stylesheet link url and div bootstrap structure

--- resources/views/article/layouts/master.blade.php
+++ resources/views/article/layouts/master.blade.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
+    
     <title>@yield('title')</title>
 </head>
 <body>
-    @yield('page_content')
+    <div class="container">
+        <div class="col-12 mt-5">
+            @yield('page_content')
+        </div>
+    </div>
+
 </body>
 </html>
